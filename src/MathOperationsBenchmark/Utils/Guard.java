04eb18d8c397230aa2a6317e436e570b8806879a
Yura95st <yura95st@gmail.com>
2015-03-05 15:47:06 +0200

    Add Guard Utility class.

---
+++ src/MathOperationsBenchmark/Utils/Guard.java
@@ -0,0 +1,38 @@
+package MathOperationsBenchmark.Utils;
+
+public class Guard
+{
+	/**
+	 * Checks if the specified argument is more or equal to zero.
+	 *
+	 * @param number
+	 *            the argument to test
+	 * @param argumentName
+	 *            the argument's name
+	 */
+	public static void isMoreOrEqualToZero(int argument, String argumentName)
+	{
+		if (argument < 0)
+		{
+			throw new IllegalArgumentException(String.format(
+				"Argument can't be less, than 0: %1$s", argumentName));
+		}
+	}
+	
+	/**
+	 * Checks if the specified argument is not null.
+	 *
+	 * @param argument
+	 *            the argument to test
+	 * @param argumentName
+	 *            the argument's name
+	 */
+	public static void isNotNull(Object argument, String argumentName)
+	{
+		if (argument == null)
+		{
+			throw new IllegalArgumentException(String.format(
+				"Argument can't be null: %1$s", argumentName));
+		}
+	}
+}
---
+++ test/MathOperationsBenchmark/Utils/GuardTests.java
@@ -0,0 +1,43 @@
+package MathOperationsBenchmark.Utils;
+
+import org.junit.Test;
+
+import MathOperationsBenchmark.Utils.Guard;
+
+public class GuardTests
+{
+	@Test(expected = IllegalArgumentException.class)
+	public void isMoreOrEqualToZero_NumberIsLessThanZero_ThrowsIllegalArgumentException()
+	{
+		int number = -1;
+		Guard.isMoreOrEqualToZero(number, "number");
+	}
+
+	@Test
+	public void isMoreOrEqualToZero_NumberisMoreOrEqualToZero_DoesNotThrowAnyException()
+	{
+		int number = 0;
+		
+		Guard.isMoreOrEqualToZero(number, "number");
+		
+		number = 1;
+		
+		Guard.isMoreOrEqualToZero(number, "number");
+	}
+	
+	@Test
+	public void isNotNull_ObjectIsNotNull_DoesNotThrowAnyException()
+	{
+		Object tempObject = new Object();
+
+		Guard.isNotNull(tempObject, "tempObject");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void isNotNull_ObjectIsNull_ThrowsIllegalArgumentException()
+	{
+		Object tempObject = null;
+
+		Guard.isNotNull(tempObject, "tempObject");
+	}
+}
