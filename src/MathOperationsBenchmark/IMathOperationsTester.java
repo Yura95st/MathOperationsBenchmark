3bc561f557263d69301213b274d67324b3195145
Yura95st <yura95st@gmail.com>
2015-03-07 23:25:09 +0200

    Add MathOperationsTester base class where testing will be performed.

---
+++ src/MathOperationsBenchmark/IMathOperationsTester.java
@@ -0,0 +1,10 @@
+package MathOperationsBenchmark;
+
+import java.util.List;
+
+import MathOperationsBenchmark.Models.BenchmarkResult;
+
+public interface IMathOperationsTester
+{
+	List<BenchmarkResult> run();
+}
---
+++ src/MathOperationsBenchmark/MathOperationsTester.java
@@ -0,0 +1,21 @@
+package MathOperationsBenchmark;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import MathOperationsBenchmark.Enums.DataType;
+import MathOperationsBenchmark.Enums.MathOperation;
+import MathOperationsBenchmark.Models.BenchmarkResult;
+
+public class MathOperationsTester implements IMathOperationsTester
+{
+	public MathOperationsTester()
+	{
+	}
+	
+	@Override
+	public List<BenchmarkResult> run()
+	{	
+		return null;
+	}
+}
